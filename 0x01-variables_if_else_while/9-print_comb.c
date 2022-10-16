3bbecd4fdf4e06869af71f4f0663a2111b0d6724
joel-mumo <mumowambua@gmail.com>
2022-10-16 16:25:40 -0700

    Task9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = 48; comb < 58; comb++)
+	{
+		putchar(comb);
+		if (comb != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+
+	return (0);
+	}
+}
