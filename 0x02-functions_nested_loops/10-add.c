046c16a2103343fa7850662847cefcdd205f3d06
joel-mumo <mumowambua@gmail.com>
2022-10-18 14:26:50 -0700

    Task10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two number
+ * @s: the first number
+ * @w: the second number
+ * Return: the sum of the two numbers
+ */
+int add(int s, int w)
+{
+	return (s + w);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is introduction to low level programming
+Functions, nested loops in C
