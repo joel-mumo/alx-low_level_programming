b6eebdd4d72effa03460ed2752940b51b54cccb3
joel-mumo <mumowambua@gmail.com>
2022-11-01 11:22:27 -0700

    Task1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-
-	for (i = 0; i < n; i++)
+	while (n-- > 0)
 	{
-		dest[i] = src[i];
+		*dest = *src;
+		dest++;
+		src++;
 	}
 
 	return (dest);
