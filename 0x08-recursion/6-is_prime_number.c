a1efb507af40f19d9ad6d7bd652ab06551e9d071
joel-mumo <mumowambua@gmail.com>
2022-11-03 14:21:23 -0700

    Task 6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _prime - recursive function
+ * @j: an integer
+ * @itt: numbers to iterate
+ * Return: 1on success, else 0
+ */
+int _prime(int j, int itt)
+{
+	if (itt == j - 1)
+	{
+		return (1);
+	}
+	else if (j % itt == 0)
+	{
+		return (0);
+	}
+	else if (j % itt != 0)
+	{
+		return (prime(j, itt + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * is_prime_number - Checks whether input is a prime number
+ * @n: input
+ * Return: 1 if prime, else 0
+ */
+int is_prime_number(int n)
+{
+	int itt;
+
+	itt = 2;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (_prime(n, itt));
+	}
+}
