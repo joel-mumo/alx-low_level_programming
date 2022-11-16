e0267d99d1a877e7ff40f50febde2899b5b101d6
joel-mumo <mumowambua@gmail.com>
2022-11-16 05:44:29 -0800

    Header files and task0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,10 @@ struct dog
 	char *owner;
 };
 
-typedef dog dog_s;
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_s *new_dog(char *name, float age, char *owner);
-void free_dog(dog_s *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
