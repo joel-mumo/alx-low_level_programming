da099fa8d588dbc083ae1cee30a7458bd65aea25
joel-mumo <mumowambua@gmail.com>
2022-11-17 06:05:09 -0800

    Task0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_name - Prints a name
@@ -8,7 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!*name || !*f)
+	if (!name || !f)
 		return;
 	f(name);
 }
