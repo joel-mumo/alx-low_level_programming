93a01ea41786dbb4b3234640764ce8c3dfbd4e8a
joel-mumo <mumowambua@gmail.com>
2022-11-29 06:52:07 -0800

    Task0 Print list and updated the list.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: String
+ * @len: Length of the string
+ * @next: oints to the next node
+ * Description: Singly list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
