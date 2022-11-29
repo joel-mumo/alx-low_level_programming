555a46353c40a206ed4e8334772a9fb348da8e57
joel-mumo <mumowambua@gmail.com>
2022-11-29 06:35:10 -0800

    header and README files for the project

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints cahracter c to stdout
+ * @c: The character
+ * Return: 1 on success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
